e963e11c741192f695539064873a5f2afa5d1b5c
AlexCerro-FE <113368042+AlexCerro-FE@users.noreply.github.com>
2023-02-20 10:58:12 +0100

    Semantic Components added

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer>
+            <div>Logo</div>
+            <div>Author</div>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main () {
+    return (
+        <main>
+            <article>
+                <h2>Book a Table</h2>
+            </article>
+        </main>
+    );
+}
+export default Main;
